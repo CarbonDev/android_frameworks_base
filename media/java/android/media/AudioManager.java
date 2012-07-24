0fce9fb751444c70d0d7caf9e3b8015e225f2b93
sethyx <sethyx@gmail.com>
2012-07-24 23:01:36 +0200

    disable camera sounds (2/3)
    Change-Id: Iaa37bc6790fe32664f8b52178c14fe926ca7d4ce
    Signed-off-by: sethyx <sethyx@gmail.com>

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -492,11 +492,17 @@ public class AudioManager {
                     direction = ADJUST_SAME;
                 } else {
                     int swapKeys = Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, 0);
+                            Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, 0);
                     int rotation = mWindowManager.getDefaultDisplay().getRotation();
-                    if (swapKeys
+                    if (swapKeys == 1 // phone or hybrid
                             && (rotation == Surface.ROTATION_90
-                            ||  rotation == Surface.ROTATION_180)) {
+                            || rotation == Surface.ROTATION_180)) {
+                        direction = keyCode == KeyEvent.KEYCODE_VOLUME_UP
+                                ? ADJUST_LOWER
+                                : ADJUST_RAISE;
+                    } else if (swapKeys == 2 // tablet
+                            && (rotation == Surface.ROTATION_180
+                            || rotation == Surface.ROTATION_270)) {
                         direction = keyCode == KeyEvent.KEYCODE_VOLUME_UP
                                 ? ADJUST_LOWER
                                 : ADJUST_RAISE;
