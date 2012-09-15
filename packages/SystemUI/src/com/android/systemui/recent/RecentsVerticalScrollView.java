cdd9edf5fd3f79cdd2216da708d5b44c9d1666a4
D4rKn3sSyS <jedga_95@outlook.com>
2012-09-15 15:00:48 -0500

    "Clear all" button on recent apps
        Ported from ParanoidAndroid by zigackly:
        https://github.com/ParanoidAndroid/android_frameworks_base/commit/f8c44e6ef667a3ebe9b658f148fa78a8d4f9a8dd
    
        Add button to tablet layout ported from Codename Android by zigackly:
        https://github.com/CNA/android_frameworks_base/commit/8d259fe3588c261baa5a17c361255cf28cc82bfe
    
        Patch set 2: Make the layout consistent, with the button at the top right.
    
        Patch set 3: Remove the redundant use of removeTask()
    
        Patch set 4: Top right layout does not work in tablet mode: make it top left for tablets only.
    
        Patch set 5: Different implementation (No need to keep track of views) - Danesh M
    
        Patch set 6: Top left layout no longer works for tablet mode with new implementation.
                     Make it bottom left for tablets only.
    
    Change-Id: Iceb86eef41160f1c17e51463b37fd97e27acfb2e
    
    Conflicts:
    	packages/SystemUI/res/layout-port/status_bar_recent_panel.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Change-Id: I5f3827299866a951e657ce64bea7c5f31c34ef9e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recent/RecentsVerticalScrollView.java

--- packages/SystemUI/src/com/android/systemui/recent/RecentsVerticalScrollView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsVerticalScrollView.java
@@ -22,6 +22,7 @@ import android.content.res.Configuration;
 import android.database.DataSetObserver;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.FloatMath;
@@ -183,6 +184,21 @@ public class RecentsVerticalScrollView extends ScrollView
     @Override
     public void removeViewInLayout(final View view) {
         dismissChild(view);
+    }
+
+    @Override
+    public void removeAllViewsInLayout() {
+        smoothScrollTo(0, 0);
+        int count = mLinearLayout.getChildCount();
+        for (int i = 0; i < count; i++) {
+            final View child = mLinearLayout.getChildAt(i);
+            postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    dismissChild(child);
+                }
+            }, i * 150);
+        }
     }
 
     public boolean onInterceptTouchEvent(MotionEvent ev) {
