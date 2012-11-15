0f9411c59d68336248de5c4331d8ce4859044e8b
Maunik Shah <mshah@codeaurora.org>
2012-11-15 14:07:54 +0530

    Framework/base: Added synchronization block to avoid race condition
    Fix for the synchronization issue leading to access of an array
    Index out of bounds. Issue occurs due to race condition between
    removing the activities of a crashed process from history stack
    and resuming a separate activity.
    
    Change-Id: I14bb5834e778c15b674248e46fe93b0ce9f37967

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -3700,7 +3700,9 @@ final class ActivityStack {
         }
 
         if (activityRemoved) {
-            resumeTopActivityLocked(null);
+            synchronized (mService) {
+                resumeTopActivityLocked(null);
+            }
         }
 
         return res;
