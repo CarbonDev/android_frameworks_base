44548b8b0aa43039282db8ea71aa5d60f1e49b7b
Roman Birg <romanbirg@gmail.com>
2012-11-22 14:03:58 -0600

    Statusbar Brightness Control (AOKP impl) (1/2)
    Find it more effective than CM...
    
    re-implement statusbar brightness slider
    
    TODO: allow it to be disabled by adding a System.Setting?
    
    Signed-off-by: Roman Birg <romanbirg@gmail.com>
    
    SystemUI: add statusbar slider preference
    
    Change-Id: Ibad11cd0495aa5fc2aa31e6f6c1e1cf85b8a582b
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4141,6 +4141,14 @@ public final class Settings {
         public static final String NAVIGATION_BAR_MENU_ARROW_KEYS = "navigation_bar_menu_arrow_keys";
 
         /**
+         * Boolean value whether to allow the statusbar to be used as a
+         * brightness slider when collapsed
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_BRIGHTNESS_SLIDER = "statusbar_brightness_slider";
+
+        /**
          * Navigation bar color.
          *
          * @hide
