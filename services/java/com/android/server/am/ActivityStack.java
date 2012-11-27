56fdadaacc77e6066f3edcfed7164788d663ecc0
Young-ho Cha <ganadist@gmail.com>
2012-11-27 11:58:06 +0900

    remove leak WaitResult List in startActivityMayWait
    Change-Id: If169ef4474a6a38eb2957c806c6e7f8f7ce67783
    Signed-off-by: Young-ho Cha <ganadist@gmail.com>

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -3230,6 +3230,7 @@ final class ActivityStack {
                         } catch (InterruptedException e) {
                         }
                     } while (!outResult.timeout && outResult.who == null);
+                    mWaitingActivityLaunched.remove(outResult);
                 } else if (res == ActivityManager.START_TASK_TO_FRONT) {
                     ActivityRecord r = this.topRunningActivityLocked(null);
                     if (r.nowVisible) {
@@ -3246,6 +3247,7 @@ final class ActivityStack {
                             } catch (InterruptedException e) {
                             }
                         } while (!outResult.timeout && outResult.who == null);
+                        mWaitingActivityVisible.remove(outResult);
                     }
                 }
             }
