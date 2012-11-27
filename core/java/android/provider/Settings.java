301007d423e826a86e0b04abb47e512ff3756d94
DvTonder <david.vantonder@gmail.com>
2012-11-27 18:08:18 -0500

    Framework: Forward port Long press back to kill app
    Original author: DaneshM
    
    Change-Id: I54f4fb861897fe713cffa15422535c5091a0c095
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/symbols.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4872,6 +4872,12 @@ public final class Settings {
         public static final String PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
 
         /**
+         * Whether to allow killing of the foreground app by long-pressing the Back button
+         * @hide
+         */
+        public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -5170,13 +5176,6 @@ public final class Settings {
          */
         public static final String WIRELESS_CHARGING_STARTED_SOUND =
                 "wireless_charging_started_sound";
-
-        /**
-         * Whether to allow killing of the foreground app by long-pressing the Back button
-         * @hide
-         */
-        public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
-
 
         /**
          * Whether we keep the device on while the device is plugged in.
