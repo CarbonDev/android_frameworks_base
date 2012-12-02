967e5d5202d07aff66b4a573040a34f13cf7922f
DvTonder <david.vantonder@gmail.com>
2012-12-02 08:20:22 -0500

    (12) Framework: Forward port the ability to enable/disable keyguard with Profiles
    This commit adds a few missing pieces needed to support the bypassing of the
    keyguard via a profile
    
    Change-Id: I557b80e6de62687b05c434a31083c331127a0398
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewMediator.java

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewMediator.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewMediator.java
@@ -22,6 +22,8 @@ import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.Profile;
+import android.app.ProfileManager;
 import android.app.SearchManager;
 import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
@@ -249,6 +251,8 @@ public class KeyguardViewMediator {
     private int mLockSoundStreamId;
 
     private int mSlideLockDelay;
+
+    private ProfileManager mProfileManager;
 
     /**
      * The volume applied to the lock/unlock sounds.
@@ -515,6 +519,8 @@ public class KeyguardViewMediator {
 
         mKeyguardViewManager = new KeyguardViewManager(context, wm, mViewMediatorCallback,
                 mLockPatternUtils);
+
+        mProfileManager = (ProfileManager) context.getSystemService(Context.PROFILE_SERVICE);
 
         mUserPresentIntent = new Intent(Intent.ACTION_USER_PRESENT);
         mUserPresentIntent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING
@@ -931,6 +937,16 @@ public class KeyguardViewMediator {
                 && mLockPatternUtils.isLockScreenDisabled() && !lockedOrMissing) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because lockscreen is off");
             return;
+        }
+
+        // if the current profile has disabled us, don't show
+        Profile profile = mProfileManager.getActiveProfile();
+        if (profile != null) {
+            if (!lockedOrMissing
+                    && profile.getScreenLockMode() == Profile.LockMode.DISABLE) {
+                if (DEBUG) Log.d(TAG, "doKeyguard: not showing because of profile override");
+                return;
+            }
         }
 
         if (DEBUG) Log.d(TAG, "doKeyguard: showing the lock screen");
