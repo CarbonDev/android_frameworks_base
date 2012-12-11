46ad7498bed4ad90507007e56becf01cb7967494
Roman Birg <romanbirg@gmail.com>
2012-12-11 11:34:17 -0600

    SystemUI: fix quick toggles layout in landscape mode
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -256,12 +256,6 @@ class QuickSettings {
         return toggleMap;
     }
 
-    // The set of QuickSettingsTiles that have dynamic spans (and need to be
-    // updated on
-    // configuration change)
-    private final ArrayList<QuickSettingsTileView> mDynamicSpannedTiles =
-            new ArrayList<QuickSettingsTileView>();
-
     private final RotationPolicy.RotationPolicyListener mRotationPolicyListener =
             new RotationPolicy.RotationPolicyListener() {
                 @Override
@@ -542,7 +536,6 @@ class QuickSettings {
                                 R.string.accessibility_quick_settings_user, state.label));
                     }
                 });
-                mDynamicSpannedTiles.add(quick);
                 break;
             case CLOCK_TILE:
                 quick = (QuickSettingsTileView)
@@ -571,7 +564,6 @@ class QuickSettings {
                          tv.setTextSize(1, mTileTextSize);
                     }
                 });
-                mDynamicSpannedTiles.add(quick);
                 break;
             case SIGNAL_TILE:
                 if (mModel.deviceSupportsTelephony()) {
@@ -645,7 +637,6 @@ class QuickSettings {
                         dismissBrightnessDialog(mBrightnessDialogShortTimeout);
                     }
                 });
-                mDynamicSpannedTiles.add(quick);
                 break;
             case SETTINGS_TILE:
                 quick = (QuickSettingsTileView)
@@ -675,7 +666,6 @@ class QuickSettings {
                         tv.setTextSize(1, mTileTextSize);
                     }
                 });
-                mDynamicSpannedTiles.add(quick);
                 break;
             case WIFI_TILE:
                 quick = (QuickSettingsTileView)
@@ -1265,7 +1255,6 @@ class QuickSettings {
                                 R.string.accessibility_quick_settings_user, state.label));
                     }
                 });
-                mDynamicSpannedTiles.add(quick);
                 break;
         }
         return quick;
@@ -1309,8 +1298,6 @@ class QuickSettings {
         if (parent.getChildCount() > 0)
             parent.removeAllViews();
         toggles = getCustomUserTiles();
-        if (mDynamicSpannedTiles.size() > 0)
-            mDynamicSpannedTiles.clear();
 
         if (!toggles.get(0).equals("")) {
             for (String toggle : toggles) {
@@ -1408,12 +1395,6 @@ class QuickSettings {
         // Update the model
         mModel.updateResources(getCustomUserTiles());
 
-        // Update the User, Time, and Settings tiles spans, and reset everything
-        // else
-        int span = r.getInteger(R.integer.quick_settings_user_time_settings_tile_span);
-        for (QuickSettingsTileView v : mDynamicSpannedTiles) {
-            v.setColumnSpan(span);
-        }
         ((QuickSettingsContainerView) mContainerView).updateResources();
         mContainerView.requestLayout();
 
