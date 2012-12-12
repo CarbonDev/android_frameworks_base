dbb7fc71341bc1114c214f84125c041452b1d1b6
StevenHarperUK <StevenHarperUK@gmail.com>
2012-12-13 00:41:32 +0100

    Ported: Volume adjust sound preference 1/2
    Author:    StevenHarperUK <StevenHarperUK@gmail.com>
    
    Feature : Volume adjust sound preference 1/2
    
    Adjusting the volume makes a nice "boing" sound
    Personally I find this annoying
    This patch makes this a preference in
    Settings | Sound
        Volume adjust sound
            Play sound when adjusting volume with hard keys
    
    The Checkbox is defaulted to ON (1 in defaults.xml)
    
    Look for 2/2 for the Settings, Sound Panel Change
    
    Change-Id: Iad89a0c49288094e21824aa9c9e1bf98c29cfec3
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3079,6 +3079,13 @@ public final class Settings {
         public static final String LOCKSCREEN_CUSTOM_TEXT_COLOR = "lockscreen_custom_text_color";
 
         /**
+         * Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
+         * Defaults to 1 - sounds enabled
+         * @hide
+         */
+        public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
+        /**
          * Boolean value whether to link ringtone and notification volumes
          *
          * @hide
