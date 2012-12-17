449afc3b6436d8510e948538cda32921b190771a
Lars Greiss <kufikugel@googlemail.com>
2012-12-17 05:11:21 -0600

    Frameworks: Statusbar clock and date customizations
    Some things changed on kitkat + our old code needed in general a bit
    rework. All done with this patchset.
    
    This commit add support for:
    
    - Clock and date enable/disable
    - Clock and date position
    - AM/PM styles multilang support
    - color
    - Date format
    - Date style
    
    Change-Id: I73d854ed5cfb57fa19e4b5554bb10cb2e1ce9b65
    
    fb: statusbar reflect am/pm based on the global user settings 12h/24h format (1/2)
    
    Change-Id: I7d6eec4d62f0e1072f11e8e131608b78f17539d9
    
    fb: do not call every time settings on clock intents
    
    only needed for configuration change.
    
    Change-Id: Ie6db0e8a3abca83f5ea6c25f39124672e06ac4ff
    
    fb: set correct providers default value am/pm
    
    fresh install shows am/pm on english timezones which
    does not fit with the aosp default behaviour. Go inline with
    it and hide it as default. User can change it then
    
    Change-Id: I503818a16772f30260222ec5beed15e6654345a1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mCircleBattery, mClock;
+    private View mLeftSide, mStatusIcons, mSignalCluster;
+    private View mBattery, mCircleBattery, mClock, mCenterClock;
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -48,6 +49,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mSignalCluster = mView.findViewById(R.id.signal_cluster);
         mBattery = mView.findViewById(R.id.battery);
         mCircleBattery = mView.findViewById(R.id.circle_battery);
+        mCenterClock = mView.findViewById(R.id.center_clock);
         mClock = mView.findViewById(R.id.clock);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
@@ -94,6 +96,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mCircleBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC)
+                    animateTransitionTo(mCenterClock, newAlphaBC)
                     );
             if (mode == MODE_LIGHTS_OUT) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -107,6 +110,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mBattery.setAlpha(newAlphaBC);
             mCircleBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
+            mCenterClock.setAlpha(newAlphaBC);
         }
     }
 }
