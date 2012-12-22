28a78aa3ef432b917effa82304af0da4089cea7b
Jorge Ruesga <jorge@ruesga.com>
2012-12-22 17:01:27 +0100

    Wi-Fi: Ping the driver for wifi-only devices
    Wifi-only devices are not capable to find APs with hidden SSID unless a DRIVER COUNTRY command
    will send to wifi driver. This change has:
    
    - Send DRIVER COUNTRY command to ping the driver (to correct the seek of hiddens SSIDs)
    - Save mCountryCode as uppercase because the wifi_countrycode_values array is in uppercase.
      Otherwise the preference will not be found.
    
    Patchset 7: Rebase
                Do not set mCountryCode if country is null
    
    Change-Id: Ie0f838aef07eaf683b713f7f0e46ff3c751523ab
    
    Conflicts:
    	wifi/java/android/net/wifi/WifiStateMachine.java

--- wifi/java/android/net/wifi/WifiNative.java
+++ wifi/java/android/net/wifi/WifiNative.java
@@ -355,6 +355,10 @@ public class WifiNative {
     }
 
     public boolean setCountryCode(String countryCode) {
+        if (countryCode == null) {
+            // Ping the driver
+            return doBooleanCommand("DRIVER COUNTRY");
+        }
         return doBooleanCommand("DRIVER COUNTRY " + countryCode);
     }
 
