3de96a09c2d100cec9dcb684bc131c984fac87e3
jt1134 <jt1134@gmail.com>
2012-12-29 14:05:08 -0600

    Option to toggle torch using power button (1/2)
    - Allows user to enable torch by holding the power
      button while the screen is off. When the power
      button is released, the torch is disabled.
    
    Change-Id: I541a823dc4e17b8c727919cdfaa96904f9a3d996
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -401,6 +401,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // HW overlays state
     int mDisableOverlays = 0;
 
+    // Power button torch
+    boolean mPowerButtonTorch;
+    boolean mTorchOn;
+
     private PowerMenuReceiver mPowerMenuReceiver;
 
     // PowerMenu Tile
@@ -731,6 +735,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HARDWARE_KEY_REBINDING), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.POWER_BUTTON_TORCH), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_SHOW), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_SHOW_NOW), false, this);
@@ -1029,6 +1035,22 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
     };
+
+    private final Runnable mTorchLongPress = new Runnable() {
+        @Override
+        public void run() {
+            toggleTorch(true); // on
+        }
+    };
+
+    void toggleTorch(boolean on) {
+        boolean bright = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.EXPANDED_FLASH_MODE, 0) == 1;
+        Intent intent = new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT");
+        intent.putExtra("bright", bright);
+        mContext.sendBroadcast(intent);
+        mTorchOn = on;
+    }
 
     void showGlobalActionsDialog() {
         if (mGlobalActions == null) {
@@ -1649,6 +1671,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.VOLUME_WAKE_SCREEN, 0, UserHandle.USER_CURRENT) == 1);
             mVolBtnMusicControls = (Settings.System.getIntForUser(resolver,
                     Settings.System.VOLBTN_MUSIC_CONTROLS, 1, UserHandle.USER_CURRENT) == 1);
+
+            mPowerButtonTorch = (Settings.System.getIntForUser(resolver,
+                    Settings.System.POWER_BUTTON_TORCH, 0, UserHandle.USER_CURRENT) == 1);
 
             boolean keyRebindingEnabled = Settings.System.getInt(resolver,
                     Settings.System.HARDWARE_KEY_REBINDING, 0) == 1;
@@ -4342,6 +4367,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         final boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
+        final boolean up = event.getAction() == KeyEvent.ACTION_UP;
         final boolean canceled = event.isCanceled();
         int keyCode = event.getKeyCode();
 
@@ -4390,7 +4416,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // When the screen is off and the key is not injected, determine whether
             // to wake the device but don't pass the key to the application.
             result = 0;
-            if (down && isWakeKey && isWakeKeyWhenScreenOff(keyCode)) {
+            if (((down && !mPowerButtonTorch) || (up && !mTorchOn && mPowerButtonTorch))
+                    && isWakeKey && isWakeKeyWhenScreenOff(keyCode)) {
                 if (keyguardActive) {
                     // If the keyguard is showing, let it wake the device when ready.
                     mKeyguardMediator.onWakeKeyWhenKeyguardShowingTq(keyCode);
@@ -4537,6 +4564,22 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_POWER: {
+                // handle power key long-press
+                if (mPowerButtonTorch && !isScreenOn) {
+                    if (down && !mTorchOn) {
+                        mHandler.postDelayed(mTorchLongPress, 1000);
+                        return 0;
+                    }
+
+                    if (up) {
+                        mHandler.removeCallbacks(mTorchLongPress);
+                        if (mTorchOn) {
+                            toggleTorch(false); // off
+                            return 0;
+                        }
+                    }
+                }
+
                 if ((mTopFullscreenOpaqueWindowState.getAttrs().flags
                         & WindowManager.LayoutParams.PREVENT_POWER_KEY) != 0){
                     return result;
