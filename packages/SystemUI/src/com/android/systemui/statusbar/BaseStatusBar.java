0551bfdc7cc24ceb4572e8ffa655ff6c8d5025b6
drcmda <drcmda@gmail.com>
2013-01-20 14:45:56 +0000

    2 chevrons and complete notification readout ;-)
    Change-Id: I6f1d30db081f23618c8b3dde2c0aa37bc0abaaed

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -189,6 +189,10 @@ public abstract class BaseStatusBar extends SystemUI implements
         return null;
     }
 
+    public NotificationData getNotificationData() {
+        return mNotificationData;
+    }
+
     public NotificationRowLayout getNotificationRowLayout() {
         return mPile;
     }
