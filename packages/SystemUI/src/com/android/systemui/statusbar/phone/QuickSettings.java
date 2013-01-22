6e38db3f470d976fc47b21078c0cad35c14843a0
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-01-22 19:16:25 +0100

    Fix NFC Tile not showing actual state after reboot
    This way if NFC is ON at boot, it will receive the ADAPTER_STATE_CHANGED intent
    and will set the adapter up, if it's OFF we'll set it in onClick; or if it gets
    turned on from another source, the above intent will be received.
    
    Change-Id: Ic8c895cc3456862c5e3bf5cafae44718703769a2
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -1019,14 +1019,17 @@ class QuickSettings {
                     public void onClick(View v) {
                         boolean enabled = false;
                         if (mNfcAdapter == null) {
-                            mNfcAdapter = NfcAdapter.getDefaultAdapter();
-                            mModel.setNfcAdapter(mNfcAdapter);
+                            mNfcAdapter = NfcAdapter.getDefaultAdapter(mContext);
                         }
-                        enabled = mNfcAdapter.isEnabled();
-                        if (enabled) {
-                            mNfcAdapter.disable();
-                        } else {
-                            mNfcAdapter.enable();
+                        try {
+                            enabled = mNfcAdapter.isEnabled();
+                            if (enabled) {
+                                mNfcAdapter.disable();
+                            } else {
+                                mNfcAdapter.enable();
+                            }
+                        } catch (NullPointerException ex) {
+                            // we'll ignore this click
                         }
                     }
                 });
