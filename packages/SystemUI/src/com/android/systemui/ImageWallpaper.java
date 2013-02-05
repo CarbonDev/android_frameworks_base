269065eb4cbfdb83007133e831afa1ea8477353e
Xianzhong <b07117@freescale.com>
2013-02-05 20:10:18 +0800

    Fixed SystemUI crash when disable GPU acceleration
    The designated logic should switch to SW Canvas render if OpenGL-ES 2.0
    is not available when GPU 3D core is disabled or non-existed.
    
    Change-Id: Ie66799bc9e1113a4b09adbb09b77021622a08bc4
    Signed-off-by: Xianzhong <b07117@freescale.com>
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
+++ packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
@@ -637,7 +637,10 @@ public class ImageWallpaper extends WallpaperService {
     
             mEglConfig = chooseEglConfig();
             if (mEglConfig == null) {
-                throw new RuntimeException("eglConfig not initialized");
+                //chooseEGLConfig fail since no egl render type EGL_OPENGL_ES2_BIT when disable gpu acceleration,
+                //should switch to SW canvas renderer without throwing exception in this case.
+                checkEglError();
+                return false;
             }
             
             mEglContext = createContext(mEgl, mEglDisplay, mEglConfig);
@@ -651,6 +654,11 @@ public class ImageWallpaper extends WallpaperService {
                 Log.e(GL_LOG_TAG, "requested  texture size " +
                     frame.width() + "x" + frame.height() + " exceeds the support maximum of " +
                     maxSize[0] + "x" + maxSize[0]);
+                return false;
+            }
+
+            if(mEglContext == EGL10.EGL_NO_CONTEXT){
+                checkEglError();
                 return false;
             }
     
