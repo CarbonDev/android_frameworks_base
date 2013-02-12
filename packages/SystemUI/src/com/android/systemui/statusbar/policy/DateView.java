bbeac5aea555033be5c67d054811c2c10f2693c4
Tylar Overturf <toverturf@gmail.com>
2013-02-11 23:09:28 -0700

    Fix tapping on clock in notifications.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
@@ -16,33 +16,22 @@
 
 package com.android.systemui.statusbar.policy;
 
-import android.app.ActivityManagerNative;
-import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Canvas;
-import android.net.Uri;
-import android.provider.CalendarContract;
 import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
 import android.view.ViewParent;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.android.systemui.R;
 
 import java.util.Date;
 
-public class DateView extends TextView implements OnClickListener, OnLongClickListener {
+public class DateView extends TextView {
     private static final String TAG = "DateView";
-
-    private RelativeLayout mParent;
 
     private boolean mAttachedToWindow;
     private boolean mWindowVisible;
@@ -62,8 +51,6 @@ public class DateView extends TextView implements OnClickListener, OnLongClickLi
 
     public DateView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setOnClickListener(this);
-        setOnLongClickListener(this);
     }
 
     @Override
@@ -77,23 +64,7 @@ public class DateView extends TextView implements OnClickListener, OnLongClickLi
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         mAttachedToWindow = false;
-        if (mParent != null) {
-            mParent.setOnClickListener(null);
-            mParent.setOnLongClickListener(null);
-            mParent = null;
-        }
         setUpdates();
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        if (mParent == null) {
-            mParent = (RelativeLayout) getParent();
-            mParent.setOnClickListener(this);
-            mParent.setOnLongClickListener(this);
-        }
-
-        super.onDraw(canvas);
     }
 
     @Override
@@ -151,45 +122,5 @@ public class DateView extends TextView implements OnClickListener, OnLongClickLi
                 mContext.unregisterReceiver(mIntentReceiver);
             }
         }
-    }
-
-    private void collapseStartActivity(Intent what) {
-        // collapse status bar
-        StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(
-                Context.STATUS_BAR_SERVICE);
-        statusBarManager.collapsePanels();
-
-        // dismiss keyguard in case it was active and no passcode set
-        try {
-            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
-        } catch (Exception ex) {
-            // no action needed here
-        }
-
-        // start activity
-        what.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        mContext.startActivity(what);
-    }
-
-    @Override
-    public void onClick(View v) {
-        long nowMillis = System.currentTimeMillis();
-
-        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
-        builder.appendPath("time");
-        ContentUris.appendId(builder, nowMillis);
-        Intent intent = new Intent(Intent.ACTION_VIEW)
-                .setData(builder.build());
-        collapseStartActivity(intent);
-    }
-
-    @Override
-    public boolean onLongClick(View v) {
-        Intent intent = new Intent("android.settings.DATE_SETTINGS");
-        intent.addCategory(Intent.CATEGORY_DEFAULT);
-        collapseStartActivity(intent);
-
-        // consume event
-        return true;
     }
 }
