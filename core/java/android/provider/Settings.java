SlimRoms/frameworks_base

Frameworks: New battery style and changes (1/2)
after a lot user requests cause of only percentage battery icon
I integrated it with some changes

- add only percentage batter icon
- add dotted circle without percentage
- abbility to colorize all percentage texts on all battery icons
- changed reset logic (slimer)
- respect theme engine
- code clean up and optimzing as usual

cheers

Change-Id: I79bac44e779d303735aad83de0c60bcc67c6c3d2

core/java/android/provider/Settings.java
@@ -2876,22 +2876,16 @@ public final class Settings {
         public static final String STATUS_BAR_CIRCLE_BATTERY_COLOR = "status_bar_circle_battery_color";
 
         /**
-         * Circle battery icon text color
+         * Battery icon text color
          * in statusbar
          */
-        public static final String STATUS_BAR_CIRCLE_BATTERY_TEXT_COLOR = "status_bar_circle_battery_text_color";
+        public static final String STATUS_BAR_BATTERY_TEXT_COLOR = "status_bar_battery_text_color";
 
         /**
          * Circle battery animation speed during charge
          * in statusbar
          */
         public static final String STATUS_BAR_CIRCLE_BATTERY_ANIMATIONSPEED = "status_bar_circle_battery_animationspeed";
-
-        /**
-         * Circle battery icon reset helper
-         * in statusbar
-         */
-        public static final String STATUS_BAR_CIRCLE_BATTERY_RESET = "status_bar_circle_battery_reset";
 
         /**
          * AM/PM Style for clock options

CarbonDev/android_frameworks_base

Conflicts:
	core/java/android/provider/Settings.java

core/java/android/provider/Settings.java
@@ -3348,22 +3348,16 @@ public final class Settings {
         public static final String STATUS_BAR_CIRCLE_BATTERY_COLOR = "status_bar_circle_battery_color";
 
         /**
-         * Circle battery icon text color
+         * Battery icon text color
          * in statusbar
          */
-        public static final String STATUS_BAR_CIRCLE_BATTERY_TEXT_COLOR = "status_bar_circle_battery_text_color";
+        public static final String STATUS_BAR_CIRCLE_BATTERY_TEXT_COLOR = "status_bar_battery_text_color";
 
         /**
          * Circle battery animation speed during charge
          * in statusbar
          */
         public static final String STATUS_BAR_CIRCLE_BATTERY_ANIMATIONSPEED = "status_bar_circle_battery_animationspeed";
-
-        /**
-         * Circle battery icon reset helper
-         * in statusbar
-         */
-        public static final String STATUS_BAR_CIRCLE_BATTERY_RESET = "status_bar_circle_battery_reset";
 
         /**
          * Control the display of the action overflow button within app UI.