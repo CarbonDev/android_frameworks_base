d48cf5ba0b2a4b16cd27181f8d5075e233d2ba2d
Lars Greiss <kufikugel@googlemail.com>
2013-02-22 05:53:58 +0100

    Fix Battery bar 2dp and 3dp graphics glitch
    and a bit codestlye
    
    cheers
    
    Change-Id: I7e01c351fdfc3e3f5adfaab9eba870a4b022cb47
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 Slimroms & CyanogenMod
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.systemui.statusbar.policy;
 
 import android.content.BroadcastReceiver;
@@ -42,7 +58,6 @@ public class BatteryBarController extends LinearLayout {
     boolean isVertical = false;
 
     class SettingsObserver extends ContentObserver {
-
         public SettingsObserver(Handler handler) {
             super(handler);
         }
@@ -125,17 +140,15 @@ public class BatteryBarController extends LinearLayout {
                     updateSettings();
                 }
             }, 500);
-
         }
     }
 
     public void addBars() {
-        // set heights
+        // Set heights
         DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
         float dp = (float) Settings.System.getInt(getContext().getContentResolver(),
                 Settings.System.STATUSBAR_BATTERY_BAR_THICKNESS, 1);
         int pixels = (int) ((metrics.density * dp) + 0.5);
-
         ViewGroup.LayoutParams params = (ViewGroup.LayoutParams) getLayoutParams();
 
         if (isVertical)
@@ -168,7 +181,6 @@ public class BatteryBarController extends LinearLayout {
                 addView(bar2, (new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
                         LayoutParams.MATCH_PARENT, 1)));
             }
-
         }
     }
 
