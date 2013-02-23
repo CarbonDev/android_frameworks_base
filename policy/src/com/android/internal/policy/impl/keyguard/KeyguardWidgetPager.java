1704e96d1ad6d704acd8b0ea4a53737aceb9b9fb
Lars Greiss <kufikugel@googlemail.com>
2013-02-23 22:25:37 +0100

    Hide statusbar clock also for DashClock
    This change hides statusbar clock now not only for cLock and default Clock
    also for DashClock
    
    cheers
    
    Change-Id: I3f1c641df74c6a800262b3dce1474f9f1107f362

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardWidgetPager.java
@@ -53,6 +53,7 @@ public class KeyguardWidgetPager extends PagedView implements PagedView.PageSwit
     private static final boolean PERFORM_OVERSCROLL_ROTATION = true;
 
     private static final String[] CLOCK_WIDGET_PACKAGES = new String[] {
+        "net.nurik.roman.dashclock",
         "com.cyanogenmod.lockclock",
         "com.android.deskclock"
     };
