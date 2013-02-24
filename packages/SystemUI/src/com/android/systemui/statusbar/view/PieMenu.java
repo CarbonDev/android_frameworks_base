0c2b7632a80d86ce518b538f80822c0ea3f5f791
drcmda <drcmda@gmail.com>
2013-02-24 01:55:22 +0000

    PIE: narrow down inner arcs for even gaps
    Change-Id: I92122efa1e2571b5f4efc84bbb9833987846dbd0

--- packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
@@ -324,11 +324,11 @@ public class PieMenu extends FrameLayout {
         float fragmentSize = 90 / CHEVRON_FRAGMENTS;
         for (int i=0; i < CHEVRON_FRAGMENTS + 1; i++) {
             mChevronPathLeft[i] = makeSlice(mPanelDegree + (i * fragmentSize), mPanelDegree + (i * fragmentSize) + fragmentSize / 2,
-                    mInnerChevronRadius, mOuterChevronRadius, mCenter);
+                    mInnerChevronRadius, mOuterChevronRadius, mCenter, 0);
         }
 
         mChevronPathRight = makeSlice(mPanelDegree + (mPanelOrientation != Gravity.TOP ? -5 : 3), mPanelDegree + 90, mInnerChevronRightRadius,
-                mOuterChevronRightRadius, mCenter);
+                mOuterChevronRightRadius, mCenter, 0);
 
         // Calculate text circle
         mStatusRadius = (int)(mResources.getDimensionPixelSize(R.dimen.pie_status_start) * mPieSize);
@@ -362,8 +362,8 @@ public class PieMenu extends FrameLayout {
 
         mStartBattery = mPanel.getDegree() + mEmptyAngle + mPieGap;
         mEndBattery = mPanel.getDegree() + (mPieGap <= 2 ? 88 : 90 - mPieGap);
-        mBatteryPathBackground = makeSlice(mStartBattery, mEndBattery, mInnerBatteryRadius, mOuterBatteryRadius, mCenter);
-        mBatteryPathJuice = makeSlice(mStartBattery, mStartBattery, mInnerBatteryRadius, mOuterBatteryRadius, mCenter);
+        mBatteryPathBackground = makeSlice(mStartBattery, mEndBattery, mInnerBatteryRadius, mOuterBatteryRadius, mCenter, 0);
+        mBatteryPathJuice = makeSlice(mStartBattery, mStartBattery, mInnerBatteryRadius, mOuterBatteryRadius, mCenter, 0);
 
         // Colors
         mEnableColor = (Settings.System.getInt(mContext.getContentResolver(),
@@ -374,11 +374,11 @@ public class PieMenu extends FrameLayout {
 
         if (mEnableColor) {
             mPieBackground.setColor(Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.PIE_BACKGROUND, COLOR_PIE_BACKGROUND));
+                        Settings.System.PIE_BACKGROUND, COLOR_PIE_BACKGROUND) | COLOR_ALPHA_MASK);
             mPieSelected.setColor(Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.PIE_SELECT, COLOR_PIE_SELECT));
+                        Settings.System.PIE_SELECT, COLOR_PIE_SELECT) | COLOR_ALPHA_MASK);
             mPieOutlines.setColor(Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.PIE_OUTLINES, COLOR_PIE_OUTLINES));
+                        Settings.System.PIE_OUTLINES, COLOR_PIE_OUTLINES) | COLOR_ALPHA_MASK);
             mClockPaint.setColor(Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.PIE_STATUS_CLOCK, COLOR_STATUS));
             mAmPmPaint.setColor(Settings.System.getInt(mContext.getContentResolver(),
@@ -386,11 +386,12 @@ public class PieMenu extends FrameLayout {
             mStatusPaint.setColor(Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.PIE_STATUS, COLOR_STATUS));
             mChevronBackgroundLeft.setColor(Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.PIE_CHEVRON_LEFT, COLOR_CHEVRON_LEFT));
+                        Settings.System.PIE_CHEVRON_LEFT, COLOR_CHEVRON_LEFT) | COLOR_OPAQUE_MASK);
             mChevronBackgroundRight.setColor(Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.PIE_CHEVRON_RIGHT, COLOR_CHEVRON_RIGHT));
+                        Settings.System.PIE_CHEVRON_RIGHT, COLOR_CHEVRON_RIGHT) | COLOR_OPAQUE_MASK);
             mBatteryJuice.setColorFilter(new PorterDuffColorFilter(extractRGB(Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.PIE_JUICE, COLOR_BATTERY_JUICE)) | COLOR_OPAQUE_MASK, Mode.SRC_ATOP));
+
             for (PieItem item : mItems) {
                 item.setColor(Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_BUTTON_COLOR, COLOR_PIE_BUTTON));
             }
@@ -405,7 +406,7 @@ public class PieMenu extends FrameLayout {
             mChevronBackgroundRight.setColor(getResources().getColor(R.color.chevron_right));
             mBatteryJuice.setColorFilter(null);
             for (PieItem item: mItems) {
-                item.setColor(COLOR_PIE_BUTTON);
+                item.setColor(getResources().getColor(R.color.status);
             }
         }
 
@@ -670,7 +671,8 @@ public class PieMenu extends FrameLayout {
 
             sweep = ((float) (Math.PI - 2 * emptyangle) / itemCount) * (item.isLesser() ? 0.65f : 1 + adjustedSweep);
             angle = (emptyangle + sweep / 2 - (float)Math.PI/2);
-            item.setPath(makeSlice(getDegrees(0) - mPieGap, getDegrees(sweep) + mPieGap, outer, inner, mCenter));
+            item.setPath(makeSlice(getDegrees(0) - mPieGap, getDegrees(sweep) + mPieGap, outer, inner, mCenter,
+                    (mPieGap > 0 ? mPieGap + 0.4f : 0)));
             View view = item.getView();
 
             if (view != null) {
@@ -726,7 +728,7 @@ public class PieMenu extends FrameLayout {
             // Special purpose animators go here
             if (mIndex == ANIMATOR_BATTERY_METER) {
                 mBatteryPathJuice = makeSlice(mStartBattery, mStartBattery + (float)animation.getAnimatedFraction() *
-                        (mBatteryLevel * (mEndBattery-mStartBattery) / 100), mInnerBatteryRadius, mOuterBatteryRadius, mCenter);
+                        (mBatteryLevel * (mEndBattery-mStartBattery) / 100), mInnerBatteryRadius, mOuterBatteryRadius, mCenter, 0);
             }
             invalidate();
         }
@@ -911,12 +913,12 @@ public class PieMenu extends FrameLayout {
         }
     }
 
-    private Path makeSlice(float start, float end, int outer, int inner, Point center) {
+    private Path makeSlice(float start, float end, int outer, int inner, Point center, float narrow) {
         RectF bb = new RectF(center.x - outer, center.y - outer, center.x + outer, center.y + outer);
         RectF bbi = new RectF(center.x - inner, center.y - inner, center.x + inner, center.y + inner);
         Path path = new Path();
         path.arcTo(bb, start, end - start, true);
-        path.arcTo(bbi, end, start - end);
+        path.arcTo(bbi, end + narrow, start - end - narrow);
         path.close();
         return path;
     }
