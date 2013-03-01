31de84878a862139ede3cf4c1d188b63d110d276
Bajee11 <bajee11@gmail.com>
2013-03-01 07:56:27 -0500

    QuietHours Toggle (1/2)
    Icon Credits: Oliver Geith
    
    Signed-off-by: Bajee11 <bajee11@gmail.com>
    Change-Id: I0fa349969115e420140f24d10302a2cdb0c91c83
    
    Conflicts:
    	packages/SystemUI/res/values/strings_toggles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -82,6 +82,7 @@ public class ToggleManager {
     public static final String SOUND_STATE_TOGGLE = "SOUNDSTATE";
     public static final String NAVBAR_HIDE_TOGGLE = "NAVBARHIDE";
     public static final String QUICKRECORD_TOGGLE = "QUICKRECORD";
+    public static final String QUIETHOURS_TOGGLE = "QUIETHOURS";
     public static final String PIE_TOGGLE = "PIE";
 
     private int mStyle;
@@ -128,6 +129,7 @@ public class ToggleManager {
             toggleMap.put(SOUND_STATE_TOGGLE, SoundStateToggle.class);
             toggleMap.put(NAVBAR_HIDE_TOGGLE, NavbarHideToggle.class);
             toggleMap.put(QUICKRECORD_TOGGLE, QuickRecordToggle.class);
+            toggleMap.put(QUIETHOURS_TOGGLE, QuietHoursToggle.class);
             toggleMap.put(PIE_TOGGLE, PieToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
