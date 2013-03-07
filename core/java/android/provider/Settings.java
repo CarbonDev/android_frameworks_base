d73dd41de053320e9bbf5a26a0749656fe4df01c
slickrick <rsalinas1@mail.usf.edu>
2013-03-07 11:19:51 -0500

    Lockscreen see through (1/2)
    -credit ParanoidAndroid

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3901,6 +3901,12 @@ public final class Settings {
         public static final String USER_UI_MODE = "user_ui_mode";
 
         /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
