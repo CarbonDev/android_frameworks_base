7e1e41a1ecba06dbc093551f93987d6df739a7e0
Lars Greiss <kufikugel@googlemail.com>
2013-03-08 19:48:53 +0100

    Recents set nicer intent flags for rambar click action
    Change-Id: Iaca1492f47667b7d260f8fd962a5fb99a6c8701f

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -913,7 +913,10 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                             ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
                         } catch (RemoteException e) {
                         }
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+                                | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+                                | Intent.FLAG_ACTIVITY_NO_HISTORY);
                         mContext.startActivityAsUser(intent, new UserHandle(UserHandle.USER_CURRENT));
                     }
                 });
@@ -931,7 +934,10 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                             ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
                         } catch (RemoteException e) {
                         }
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+                                | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+                                | Intent.FLAG_ACTIVITY_NO_HISTORY);
                         mContext.startActivityAsUser(intent, new UserHandle(UserHandle.USER_CURRENT));
                         return true;
                     }
