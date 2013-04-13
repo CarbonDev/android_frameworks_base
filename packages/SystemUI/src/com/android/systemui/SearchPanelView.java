a8fe92de48f2456c1cbdee5e3d222369adb2c5d1
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-04-13 21:21:16 +0700

    SystemUI: fix memory leaks
    ContentObservers are being registered without being unregistered,
    causing old objects not to be garbage collected. This causes memory
    leak if the classes are destroyed and recreated throughout the
    device's uptime.
    
    SearchPanelView is recreated every time the screen is turned on.
    StatusBarIconView is created every time a notification is triggered.
    
    JIRA: CYAN-650
    Change-Id: Iabd3221fc2b0c848396af1790fd09424be602e7e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/SearchPanelView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -108,6 +108,7 @@ public class SearchPanelView extends FrameLayout implements
     private final Context mContext;
     private BaseStatusBar mBar;
     private StatusBarTouchProxy mStatusBarTouchProxy;
+    private SettingsObserver mObserver;
 
     private boolean mShowing;
     private View mSearchTargetsContainer;
@@ -148,8 +149,7 @@ public class SearchPanelView extends FrameLayout implements
         mResources = mContext.getResources();
 
         mContentResolver = mContext.getContentResolver();
-        mSettingsObserver = new SettingsObserver(new Handler());
-        updateSettings();
+        mObserver = new SettingsObserver(new Handler());
     }
 
     @Override
@@ -300,9 +300,6 @@ public class SearchPanelView extends FrameLayout implements
         // TODO: fetch views
         mGlowPadView = (GlowPadView) findViewById(R.id.glow_pad_view);
         mGlowPadView.setOnTriggerListener(mGlowPadViewListener);
-
-        updateSettings();
-        setDrawables();
     }
 
     private void maybeSkipKeyguard() {
@@ -519,6 +516,21 @@ public class SearchPanelView extends FrameLayout implements
         return true;
     }
 
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        mObserver.observe();
+        updateSettings();
+        setDrawables();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mObserver.unobserve();
+    }
+
     /**
      * Whether the panel is showing, or, if it's animating, whether it will be
      * when the animation is done.
@@ -610,7 +622,10 @@ public class SearchPanelView extends FrameLayout implements
                 resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.SYSTEMUI_NAVRING_ICON[i]), false, this);
             }
+        }
 
+        void unobserve() {
+            mContext.getContentResolver().unregisterContentObserver(this);
         }
 
         @Override
