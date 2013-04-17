17edd39e9c5447bce2d1ef2db6e9dc415562f48c
blk_jack <jcorcoran+github@gmail.com>
2013-04-17 18:46:59 -0400

    MediaScanner behavior on boot (1/3)
    Lets you enable/disable/ask (with a notification) media scanning on boot.
    Can speed up boot and reduce initial boot lag from MediaScanner
    
    Change-Id: I634ae96b1dcb493b966b4653589cd97523b63da7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3193,6 +3193,15 @@ public final class Settings {
         public static final String GESTURES_UP_SWIPE = "gestures_up_swipe";
 
         /**
+         * MediaScanner behavior on boot.
+         * 0 = enabled
+         * 1 = ask (notification)
+         * 2 = disabled
+         * @hide
+         */
+         public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+
+        /**
          * down Swipe Action
          *
          * @hide
