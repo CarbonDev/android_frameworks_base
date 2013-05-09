e7df6d5653108e6e046eca7d9d24dff70bd67175
Stevespear426 <stephen.k.spear@gmail.com>
2013-05-09 17:37:09 +0200

    FW: Ribbon v1.0 overhaul
    Thickness Increase max && decrease min
    Configurable long swipe and long press swipe areas
    
    consolidate and cache views for smoother animations
    User adjustable animantion duration
    
    Add dismiss options for swipe ribbon
     - Always dismiss on click
     - never dismiss on click
     - Don't dismiss for navbar action
    
    fix app window crash if user has fast fingers and to much time
      on their hands...
    
    Swipe ribbon drag handles now get their own location, height,
       width, and opacity.
    
    Hide apps from app window
    Hooks to dismiss app window if softkeys or navbar buttons pressed
    
    Fix app window to pull all components also IE camera and messenger.
    
    Rub some unicorn polish on the on touch glow. The giant boxes were
    a bit ridiculous.
    
    If ribbon is empty but toogles are not, just show toggles no flipper.
    
    add adb wireless to toggle map
    
    Phase 1 of the Animation chooser. Simple helpers to let users
    choose the animation for the swipe ribbon and app window.
    ill be adding some fancier animations for now just some of
    the stock AOSP one.
    
    Add lots o animation options. Also preload animations instead
    of creating them at run time, should smooth things out a bit.
    
    Keyboard IME hide, Vertical spacing App window, Cache resources.
    
    Fix IME hide when navbar hidden.
    
    Longpress on app window now takes you to app info.
    
    Change-Id: I7c59fc915473fe320639eda446a735dee8500efa
    
    Conflicts:
    	core/java/com/android/internal/util/carbon/AokpRibbonHelper.java
    	core/java/com/android/internal/util/carbon/RibbonTarget.java
    	core/res/res/values/strings.xml
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/src/com/android/systemui/carbon/AokpSwipeRibbon.java
    	packages/SystemUI/src/com/android/systemui/carbon/AppWindow.java
    	packages/SystemUI/src/com/android/systemui/carbon/RibbonGestureCatcherView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1778,6 +1778,10 @@ public class PhoneStatusBar extends BaseStatusBar {
         final int diff = state ^ old;
         mDisabled = state;
 
+        mAokpSwipeRibbonBottom.setDisabledFlags(state);
+        mAokpSwipeRibbonLeft.setDisabledFlags(state);
+        mAokpSwipeRibbonRight.setDisabledFlags(state);
+
         if (DEBUG) {
             Slog.d(TAG, String.format("disable: 0x%08x -> 0x%08x (diff: 0x%08x)",
                 old, state, diff));
@@ -2171,7 +2175,7 @@ public class PhoneStatusBar extends BaseStatusBar {
                     Settings.System.getBoolean(cr,
                          Settings.System.RIBBON_ICON_VIBRATE[AokpRibbonHelper.QUICK_SETTINGS], true),
                     Settings.System.getBoolean(cr,
-                         Settings.System.RIBBON_ICON_COLORIZE[AokpRibbonHelper.QUICK_SETTINGS], false)));
+                         Settings.System.RIBBON_ICON_COLORIZE[AokpRibbonHelper.QUICK_SETTINGS], false), 0));
             }
             mRibbonNotif.removeAllViews();
             mRibbonNotif.addView(AokpRibbonHelper.getRibbon(mContext,
@@ -2192,7 +2196,7 @@ public class PhoneStatusBar extends BaseStatusBar {
                 Settings.System.getBoolean(cr,
                      Settings.System.RIBBON_ICON_VIBRATE[AokpRibbonHelper.NOTIFICATIONS], true),
                 Settings.System.getBoolean(cr,
-                     Settings.System.RIBBON_ICON_COLORIZE[AokpRibbonHelper.NOTIFICATIONS], false)));
+                     Settings.System.RIBBON_ICON_COLORIZE[AokpRibbonHelper.NOTIFICATIONS], false), 0));
     }
 
     public void updateRibbon() {
@@ -2658,6 +2662,7 @@ public class PhoneStatusBar extends BaseStatusBar {
 
     @Override // CommandQueue
     public void setNavigationIconHints(int hints) {
+
         if (hints == mNavigationIconHints) return;
 
         mNavigationIconHints = hints;
@@ -2787,6 +2792,10 @@ public class PhoneStatusBar extends BaseStatusBar {
     public void setImeWindowStatus(IBinder token, int vis, int backDisposition) {
         boolean altBack = (backDisposition == InputMethodService.BACK_DISPOSITION_WILL_DISMISS)
             || ((vis & InputMethodService.IME_VISIBLE) != 0);
+
+        mAokpSwipeRibbonBottom.setNavigationIconHints(vis);
+        mAokpSwipeRibbonLeft.setNavigationIconHints(vis);
+        mAokpSwipeRibbonRight.setNavigationIconHints(vis);
 
         mCommandQueue.setNavigationIconHints(
                 altBack ? (mNavigationIconHints | StatusBarManager.NAVIGATION_HINT_BACK_ALT)
