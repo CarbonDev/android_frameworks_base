240a98258cea24a1fe2b9e3059a830fa56063daf
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-05-19 23:39:20 +0200

    LockScreen: Fix crash when unlocking twice
    This will keep track if the BroadCastReceiver is registered and
    will unregister it only if so.
    
    Change-Id: I040d710cf0927394f36b2e3da251ac5a988ce1d6
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java
@@ -90,6 +90,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
     private String[] customIcons = new String[8];
     private UnlockReceiver receiver;
     private IntentFilter filter;
+    private boolean mReceiverRegistered = false;
 
     private class H extends Handler {
         public void handleMessage(Message m) {
@@ -146,7 +147,10 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
         };
 
         public void onTrigger(View v, int target) {
-            mContext.unregisterReceiver(receiver);
+            if (mReceiverRegistered) {
+                mContext.unregisterReceiver(receiver);
+                mReceiverRegistered = false;
+            }
             if ((!mUsesCustomTargets) || (mTargetCounter() == 0 && mUnlockCounter() < 2)) {
                 mCallback.userActivity(0);
                 mCallback.dismiss(false);
@@ -289,6 +293,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
         filter.addAction(UnlockReceiver.ACTION_UNLOCK_RECEIVER);
         receiver = new UnlockReceiver();
         mContext.registerReceiver(receiver, filter);
+        mReceiverRegistered = true;
     }
 
     public void setCarrierArea(View carrierArea) {
@@ -548,7 +553,10 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
                     mCallback.dismiss(false);
                 }
             }
-            mContext.unregisterReceiver(receiver);
+            if (mReceiverRegistered) {
+                mContext.unregisterReceiver(receiver);
+                mReceiverRegistered = false;
+            }
         }
     }
 }
