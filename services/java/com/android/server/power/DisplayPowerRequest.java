ed605579aa41d8f007b5a392850fce1be5870743
Danny Baumann <dannybaumann@web.de>
2013-05-26 13:35:04 +0200

    Improve auto-brightness handling
    - Add a setting to control the auto-brightness algorithm responsitivity
    - Make the algorithm react much faster when the ambient brightness
      increases quickly, e.g. when receiving a call in bright sunlight.
    
    Change-Id: Ic608901c476609dde69cae982228c09567140a83
    
    Conflicts:
    	services/java/com/android/server/power/DisplayPowerRequest.java
    	services/java/com/android/server/power/PowerManagerService.java

--- services/java/com/android/server/power/DisplayPowerRequest.java
+++ services/java/com/android/server/power/DisplayPowerRequest.java
@@ -66,6 +66,11 @@ final class DisplayPowerRequest {
     public boolean electronBeamOnEnabled;
     public boolean electronBeamOffEnabled;    
 
+    // Multiplication factor for delays used in auto-brightness computations
+    // Lower values mean faster reaction to changing light conditions, but
+    // potentially higher CPU usage and flicker.
+    public float responsitivityFactor;
+
     public DisplayPowerRequest() {
         screenState = SCREEN_STATE_BRIGHT;
         useProximitySensor = false;
@@ -73,6 +78,7 @@ final class DisplayPowerRequest {
         screenAutoBrightnessAdjustment = 0.0f;
         useAutoBrightness = false;
         blockScreenOn = false;
+        responsitivityFactor = 1.0f;
         electronBeamOnEnabled = false;
         electronBeamOffEnabled = false;
     }
@@ -88,6 +94,7 @@ final class DisplayPowerRequest {
         screenAutoBrightnessAdjustment = other.screenAutoBrightnessAdjustment;
         useAutoBrightness = other.useAutoBrightness;
         blockScreenOn = other.blockScreenOn;
+        responsitivityFactor = other.responsitivityFactor;
         electronBeamOnEnabled = other.electronBeamOnEnabled;
         electronBeamOffEnabled = other.electronBeamOffEnabled;
     }
@@ -106,6 +113,7 @@ final class DisplayPowerRequest {
                 && screenAutoBrightnessAdjustment == other.screenAutoBrightnessAdjustment
                 && useAutoBrightness == other.useAutoBrightness
                 && blockScreenOn == other.blockScreenOn
+                && Math.abs(responsitivityFactor - other.responsitivityFactor) < 1E-6
                 && electronBeamOnEnabled == other.electronBeamOnEnabled
                 && electronBeamOffEnabled == other.electronBeamOffEnabled;
     }
@@ -123,6 +131,7 @@ final class DisplayPowerRequest {
                 + ", screenAutoBrightnessAdjustment=" + screenAutoBrightnessAdjustment
                 + ", useAutoBrightness=" + useAutoBrightness
                 + ", blockScreenOn=" + blockScreenOn
+                + ", responsitivityFactor=" + responsitivityFactor
                 + ", electronBeamOnEnabled=" + electronBeamOnEnabled
                 + ", electronBeamOffEnabled=" + electronBeamOffEnabled;
     }
