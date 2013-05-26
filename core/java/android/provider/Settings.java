ee489003b2e15ca26a21604721d532ac27eace2b
Sam Mortimer <sam@mortimer.me.uk>
2013-05-26 11:28:45 -0700

    [2/2] Permit home button wake to be configurable
    Change-Id: Ie52247d5c23c8f840ababc880dd41992049cefd1
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3024,6 +3024,12 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
         /**
+         * Whether to wake the screen with the home key, the value is boolean.
+         * @hide
+         */
+        public static final String HOME_WAKE_SCREEN = "home_wake_screen";
+
+        /**
          * Whether or not volume button music controls should be enabled to seek media tracks
          * @hide
          */
