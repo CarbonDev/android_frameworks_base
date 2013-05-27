2e6239be7c9c8e86695d139730f66def0abcfcd3
blk_jack <jcorcoran+github@gmail.com>
2013-05-27 16:35:21 -0400

    Camera button rebinding and lockscreen action additions (1/2)
    Adds new config_deviceHardwareKeys bitmask for camera button.
    Lets you rebind the camera press/longpress and adds a new KEYCODE_CAMERA handler to inject a longpress KeyEvent
    in order to let you bind keys to actually trigger the shutter, in addition to launching the app itself.
    
    This also adds KEYCODE_ASSIST and KEYCODE_APP_SWITCH to the available keys to assign longpress lockscreen actions to.
    
    Change-Id: I63583dbbaa0f6a3d21724f4948f86bb01e44b71b
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3063,6 +3063,24 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_LONG_MENU_ACTION = "lockscreen_long_menu_action";
 
+        /**
+         * Action for long-pressing assist button on lock screen
+         * @hide
+         */
+        public static final String LOCKSCREEN_LONG_ASSIST_ACTION = "lockscreen_long_assist_action";
+
+        /**
+         * Action for long-pressing app switch button on lock screen
+         * @hide
+         */
+        public static final String LOCKSCREEN_LONG_APP_SWITCH_ACTION = "lockscreen_long_app_switch_action";
+
+        /**
+         * Action for long-pressing camera button on lock screen
+         * @hide
+         */
+        public static final String LOCKSCREEN_LONG_CAMERA_ACTION = "lockscreen_long_camera_action";
+
          /**
           * Always show the battery status on the lockscreen
           * @hide
@@ -3213,6 +3231,7 @@ public final class Settings {
           * 11 - Kill App
           * 12 - Last App
           * 13 - Custom App
+          * 14 - Camera button
           * @hide
           */
 
@@ -3281,6 +3300,20 @@ public final class Settings {
           * @hide
           */
          public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
+
+         /**
+          * Action to perform when the camera key is pressed. (Default is 16)
+          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+          * @hide
+          */
+         public static final String KEY_CAMERA_ACTION = "key_camera_action";
+
+         /**
+          * Action to perform when the app camera is long-pressed. (Default is 0)
+          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+          * @hide
+          */
+         public static final String KEY_CAMERA_LONG_PRESS_ACTION = "key_camera_long_press_action";
 
         /**
          * Whether to show the battery bar
