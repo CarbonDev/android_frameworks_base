36cd040b5d0ce7705050afa33606f558a450cf35
winner00 <toverturf@gmail.com>
2013-05-27 21:06:24 -0600

    Revert "Frameworks & SystemUI - additional DC-HSPAP support"
    This reverts commit dc8bf3a7ad74aadb071405266012b9561fec0d86.
    
    Conflicts:
    	telephony/java/android/telephony/TelephonyManager.java
    
    Change-Id: Idb6eff69f498ae6f9931ffba7bbd16f923dce5b5

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -586,10 +586,6 @@ public class TelephonyManager {
     public static final int NETWORK_TYPE_EHRPD = 14;
     /** Current network is HSPA+ */
     public static final int NETWORK_TYPE_HSPAP = 15;
-    /** Current network is DC-HSPAP
-     * @hide
-     */
-    public static final int NETWORK_TYPE_DCHSPAP = 30;
 
     /**
      * Returns a constant indicating the radio technology (network type)
@@ -612,7 +608,6 @@ public class TelephonyManager {
      * @see #NETWORK_TYPE_LTE
      * @see #NETWORK_TYPE_EHRPD
      * @see #NETWORK_TYPE_HSPAP
-     * @see #NETWORK_TYPE_DCHSPAP
      */
     public int getNetworkType() {
         try{
@@ -664,7 +659,6 @@ public class TelephonyManager {
             case NETWORK_TYPE_EVDO_B:
             case NETWORK_TYPE_EHRPD:
             case NETWORK_TYPE_HSPAP:
-            case NETWORK_TYPE_DCHSPAP:
                 return NETWORK_CLASS_3_G;
             case NETWORK_TYPE_LTE:
                 return NETWORK_CLASS_4_G;
@@ -717,8 +711,6 @@ public class TelephonyManager {
                 return "iDEN";
             case NETWORK_TYPE_HSPAP:
                 return "HSPA+";
-            case NETWORK_TYPE_DCHSPAP:
-                return "DCHSPAP";
             default:
                 return "UNKNOWN";
         }
