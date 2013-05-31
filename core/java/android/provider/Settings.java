48729d2d469839f684ebbf2f1047324d3a76ede3
BigBrother1984 <stevewatersy@gmail.com>
2013-05-31 22:09:00 +0200

    Pie: set Big as default for item gap (2/2)
    Screen: http://goo.gl/aocZ7
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: Ibb6557d2536925927b4219d00f5f2e5abb40175f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3556,7 +3556,7 @@ public final class Settings {
         public static final String PIE_APP_WINDOW = "pie_app_window";
 
         /*
-         * Pie gap angle, should default to 3
+         * Pie gap angle, should default to 2
          * @hide
          */
         public static final String PIE_GAP = "pie_gap";
