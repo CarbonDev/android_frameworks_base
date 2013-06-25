121a9ea94cc8eda12ea9ad6eb7599f4805abfa67
Daniel MM <daxx4182@gmail.com>
2013-06-25 17:50:54 +0800

    HALO : Ability to enable/disable custom colors
    Why? Because, we all know some themers might theme the halo bg or also maybe the speech bubbles, so we trying not to mess with the alternate colors... ;)
    
    Also, the drawable are being color filtered...

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2796,6 +2796,12 @@ public final class Settings {
         public static final String HALO_STYLE = "halo_style";
 
         /**
+         * HALO colors
+         * @hide
+         */
+        public static final String HALO_COLORS = "halo_colors";
+
+        /**
          * HALO speech bubble color
          * @hide
          */
