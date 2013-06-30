c3402b76cbd0c79b47f5da85e9d29b1b309400ea
Danny Baumann <dannybaumann@web.de>
2013-06-30 14:47:44 +0200

    Delay closing the notification area after swiping a little closer.
    Use the same value as when removing all notifications.
    
    Change-Id: Id11a48bc47487fd7763503dcbc769d9dbf5ad16a

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1379,7 +1379,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 public void run() {
                     animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
                 }
-            }, 100);
+            }, 225);
         }
 
         return entry.notification;
