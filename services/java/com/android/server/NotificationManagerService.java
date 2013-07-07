c55100ebbfd946c352c76fd7dfe0847272f57d60
Koushik Dutta <koushd@gmail.com>
2013-07-07 09:28:42 -0700

    Notification cancellation fixes: Permission should only allow cancellation of other app notifications, not enqueueing. Fix checkapi breakage.
    Change-Id: Idbcfd4617057984250dc4d4fa32ff55bfbb155e2

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -1651,7 +1651,7 @@ public class NotificationManagerService extends INotificationManager.Stub
     }
 
     public void cancelNotificationWithTag(String pkg, String tag, int id, int userId) {
-        checkCallerIsSystemOrSameApp(pkg);
+        checkCallerCanCancelNotification(pkg);
         userId = ActivityManager.handleIncomingUser(Binder.getCallingPid(),
                 Binder.getCallingUid(), userId, true, false, "cancelNotificationWithTag", pkg);
         // Don't allow client applications to cancel foreground service notis.
@@ -1661,7 +1661,7 @@ public class NotificationManagerService extends INotificationManager.Stub
     }
 
     public void cancelAllNotifications(String pkg, int userId) {
-        checkCallerIsSystemOrSameApp(pkg);
+        checkCallerCanCancelNotification(pkg);
 
         userId = ActivityManager.handleIncomingUser(Binder.getCallingPid(),
                 Binder.getCallingUid(), userId, true, false, "cancelAllNotifications", pkg);
@@ -1679,11 +1679,15 @@ public class NotificationManagerService extends INotificationManager.Stub
         throw new SecurityException("Disallowed call for uid " + uid);
     }
 
-    void checkCallerIsSystemOrSameApp(String pkg) {
+    void checkCallerCanCancelNotification(String pkg) {
         if (mContext.checkCallingOrSelfPermission(android.Manifest.permission.CANCEL_NOTIFICATIONS)
                 == PackageManager.PERMISSION_GRANTED) {
             return;
         }
+        checkCallerIsSystemOrSameApp(pkg);
+    }
+
+    void checkCallerIsSystemOrSameApp(String pkg) {
         int uid = Binder.getCallingUid();
         if (UserHandle.getAppId(uid) == Process.SYSTEM_UID || uid == 0) {
             return;
