a22ec91019ebcecc0773116b371f93c099652a76
drcmda <drcmda@gmail.com>
2013-07-20 15:34:12 +0100

    HALO dismiss/flip fix, clearall/last-notif-empty fix
    Change-Id: I56eeb6891af2130584620d9bc596c3f0295f2402
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -628,6 +628,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
 
                     if (mCurrentNotficationEntry == null) clearTicker();
                     mLastNotificationEntry = null;
+                    loadLastNotification(true);
 
                     mEffect.nap(1500);
                     if (mHideTicker) mEffect.sleep(HaloEffect.NAP_TIME + 3000, HaloEffect.SLEEP_TIME, false);
@@ -1210,7 +1211,8 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
 
     void clearTicker() {
         mEffect.mHaloIcon.setImageDrawable(null);
-        mEffect.setHaloMessageNumber(0, false);
+        mEffect.msgNumberAlphaAnimator.cancel(true);
+        mEffect.mHaloNumber.setAlpha(0f);
         mContentIntent = null;
         mCurrentNotficationEntry = null;
         mEffect.killTicker();
