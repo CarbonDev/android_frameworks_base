5f336b53ce98b16cbbc8abf94d5d387998acacba
winner00 <toverturf@gmail.com>
2013-07-26 18:57:01 -0600

    derp
    Change-Id: I4cf28df3a4878a98fbf6ab227e9e899ce86b7770

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4141,7 +4141,7 @@ public final class Settings {
             MOUNT_UMS_PROMPT,
             MOUNT_UMS_NOTIFY_ENABLED,
             UI_NIGHT_MODE,
-            DIALPAD_AUTOCOMPLETE
+            DIALPAD_AUTOCOMPLETE,
             PRIVACY_GUARD_DEFAULT
         };
 
