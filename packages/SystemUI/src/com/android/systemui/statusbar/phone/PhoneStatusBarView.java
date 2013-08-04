AOKP/frameworks_base

SystemUI: animate transparency changes
Change-Id: I597b245b2d98ae42118efc1ba7ccb837edfdbea9
Signed-off-by: Roman Birg <romanbirg@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -46,8 +46,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 
+import com.android.internal.util.aokp.BackgroundAlphaColorDrawable;
 import com.android.systemui.R;
-import com.android.systemui.statusbar.BackgroundAlphaColorDrawable;
 import com.android.systemui.statusbar.NavigationBarView;
 
 public class PhoneStatusBarView extends PanelBar {

CarbonDev/android_frameworks_base

Conflicts:
	core/java/com/android/internal/util/aokp/BackgroundAlphaColorDrawable.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -48,8 +48,8 @@ import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 
 import com.android.systemui.EventLogTags;
+import com.android.internal.util.carbon.BackgroundAlphaColorDrawable;
 import com.android.systemui.R;
-import com.android.systemui.statusbar.BackgroundAlphaColorDrawable;
 import com.android.systemui.statusbar.NavigationBarView;
 
 public class PhoneStatusBarView extends PanelBar {