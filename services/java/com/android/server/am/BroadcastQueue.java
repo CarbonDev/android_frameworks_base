e64d350bf0c03d91cee9671bb2bbd67b82e9a676
Danesh M <daneshm90@gmail.com>
2013-08-06 14:56:17 -0700

    BroadcastReceiver : Get sender of broadcasts
    Add internal api for receiving the sender of a broadcast
    
    Change-Id: I28ae614ef92101ab6b9beadc3c965d16b1f393f6

--- services/java/com/android/server/am/BroadcastQueue.java
+++ services/java/com/android/server/am/BroadcastQueue.java
@@ -121,6 +121,11 @@ public class BroadcastQueue {
     BroadcastRecord mPendingBroadcast = null;
 
     /**
+     * Intent broadcast that we are currently processing
+     */
+    BroadcastRecord mCurrentBroadcast = null;
+
+    /**
      * The receiver index that is pending, to restart the broadcast if needed.
      */
     int mPendingBroadcastRecvIndex;
@@ -480,6 +485,10 @@ public class BroadcastQueue {
         }
     }
 
+    BroadcastRecord getProcessingBroadcast() {
+        return mCurrentBroadcast;
+    }
+
     final void processNextBroadcast(boolean fromMsg) {
         synchronized(mService) {
             BroadcastRecord r;
@@ -500,6 +509,7 @@ public class BroadcastQueue {
                 r = mParallelBroadcasts.remove(0);
                 r.dispatchTime = SystemClock.uptimeMillis();
                 r.dispatchClockTime = System.currentTimeMillis();
+                mCurrentBroadcast = r;
                 final int N = r.receivers.size();
                 if (DEBUG_BROADCAST_LIGHT) Slog.v(TAG, "Processing parallel broadcast ["
                         + mQueueName + "] " + r);
@@ -511,6 +521,7 @@ public class BroadcastQueue {
                     deliverToRegisteredReceiverLocked(r, (BroadcastFilter)target, false);
                 }
                 addBroadcastToHistoryLocked(r);
+                mCurrentBroadcast = null;
                 if (DEBUG_BROADCAST_LIGHT) Slog.v(TAG, "Done with parallel broadcast ["
                         + mQueueName + "] " + r);
             }
@@ -560,6 +571,7 @@ public class BroadcastQueue {
                     return;
                 }
                 r = mOrderedBroadcasts.get(0);
+                mCurrentBroadcast = r;
                 boolean forceReceive = false;
 
                 // Ensure that even if something goes awry with the timeout
@@ -632,6 +644,7 @@ public class BroadcastQueue {
                     // ... and on to the next...
                     addBroadcastToHistoryLocked(r);
                     mOrderedBroadcasts.remove(0);
+                    mCurrentBroadcast = null;
                     r = null;
                     looped = true;
                     continue;
