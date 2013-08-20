597f120bb6020184922facc0a88c888bbcd98876
Daniel MM <daxx4182@gmail.com>
2013-08-20 21:32:43 +0800

    PowerWidget: Fix SysUI FC on Sleep button

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/SleepButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/SleepButton.java
@@ -20,9 +20,9 @@ public class SleepButton extends PowerButton {
 
     @Override
     protected void toggleState(Context context) {
-        PowerManager pm = (PowerManager)
+       PowerManager pm = (PowerManager)
                 context.getSystemService(Context.POWER_SERVICE);
-        pm.goToSleep(SystemClock.uptimeMillis() + 1);
+        pm.goToSleep(SystemClock.uptimeMillis());
     }
 
     @Override
