69b60809c4c899bb56062f233c9b8316b7788cd1
winner00 <toverturf@gmail.com>
2013-08-26 23:46:58 -0600

    Revert "wifi: Set the mode at native layer"
    Was causing boot issues on mako.
    
    This reverts commit 01a119188dd3e1c0a7274f46148cba3c2126d3ea.

--- wifi/java/android/net/wifi/WifiNative.java
+++ wifi/java/android/net/wifi/WifiNative.java
@@ -803,6 +803,4 @@ public class WifiNative {
         // Note: optional feature on the driver. It is ok for this to fail.
         doBooleanCommand("DRIVER MIRACAST " + mode);
     }
-
-    public native static boolean setMode(int mode);
 }
