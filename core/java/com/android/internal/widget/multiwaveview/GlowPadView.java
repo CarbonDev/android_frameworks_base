66b2557b8fc58e3290645ddbaefca91cf5ed5420
nuclearmistake <nuclearmistake@gmail.com>
2013-09-26 09:38:50 -0400

    Active-D: Make GlowPadView center the current notification icon for us
    sudo makemeasandwich
    
    ps2: make scaling both smarter and stupider SIMULTANEOUSLY
    
    Change-Id: I582f387fd3c1c83f2bbd0d31a4b43e22d1b82d63
    Signed-off-by: nuclearmistake <nuclearmistake@gmail.com>

--- core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
+++ core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
@@ -148,6 +148,8 @@ public class GlowPadView extends View {
     private TargetDrawable mOuterRing;
     private Vibrator mVibrator;
 
+    private TargetDrawable mCenterDrawable;
+
     private int mFeedbackCount = 3;
     private int mVibrationDuration = 0;
     private int mGrabbedState;
@@ -282,6 +284,8 @@ public class GlowPadView extends View {
         TypedValue handle = a.peekValue(R.styleable.GlowPadView_handleDrawable);
         mHandleDrawable = new TargetDrawable(res, handle != null ? handle.resourceId : 0);
         mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
+        mCenterDrawable = new TargetDrawable(res, 0);
+        mCenterDrawable.setState(TargetDrawable.STATE_INACTIVE);
         mOuterRing = new TargetDrawable(res,
                 getResourceId(a, R.styleable.GlowPadView_outerRingDrawable));
 
@@ -431,6 +435,8 @@ public class GlowPadView extends View {
                 startBackgroundAnimation(0, 0.0f);
                 mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
                 mHandleDrawable.setAlpha(1.0f);
+                mCenterDrawable.setState(TargetDrawable.STATE_INACTIVE);
+                mCenterDrawable.setAlpha(1.0f);
                 mPaintText.setAlpha(255);
                 break;
 
@@ -441,6 +447,7 @@ public class GlowPadView extends View {
             case STATE_FIRST_TOUCH:
                 mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
+                mCenterDrawable.setAlpha(0.0f);
                 deactivateTargets();
                 showTargets(true);
                 startBackgroundAnimation(INITIAL_SHOW_HANDLE_DURATION, 1.0f);
@@ -453,6 +460,7 @@ public class GlowPadView extends View {
             case STATE_TRACKING:
                 mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
+                mCenterDrawable.setAlpha(0.0f);
                 showGlow(REVEAL_GLOW_DURATION , REVEAL_GLOW_DELAY, 1.0f, null);
                 break;
 
@@ -460,6 +468,7 @@ public class GlowPadView extends View {
                 // TODO: Add transition states (see list_selector_background_transition.xml)
                 mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
+                mCenterDrawable.setAlpha(0.0f);
                 showGlow(REVEAL_GLOW_DURATION , REVEAL_GLOW_DELAY, 0.0f, null);
                 break;
 
@@ -1239,6 +1248,8 @@ public class GlowPadView extends View {
 
         mHandleDrawable.setPositionX(newWaveCenterX);
         mHandleDrawable.setPositionY(newWaveCenterY);
+        mCenterDrawable.setPositionX(newWaveCenterX);
+        mCenterDrawable.setPositionY(newWaveCenterY);
 
         updateTargetPositions(newWaveCenterX, newWaveCenterY);
         updatePointCloudPosition(newWaveCenterX, newWaveCenterY);
@@ -1306,6 +1317,7 @@ public class GlowPadView extends View {
                 target.draw(canvas);
             }
         }
+        mCenterDrawable.draw(canvas);
         mHandleDrawable.draw(canvas);
 
         if (!TextUtils.isEmpty(mHandleText) && mPaintText.getAlpha() != 0) {
@@ -1330,6 +1342,12 @@ public class GlowPadView extends View {
         mHandleText = TextUtils.ellipsize(
                 text, new TextPaint(mPaintText), mMaxTextArcLength,
                 TextUtils.TruncateAt.END).toString();
+    }
+
+    public void setCenterDrawable(TargetDrawable d) {
+        mCenterDrawable = new TargetDrawable(d);
+        mCenterDrawable.setPositionX(mWaveCenterX);
+        mCenterDrawable.setPositionY(mWaveCenterY);
     }
 
     public void setDrawOuterRing(boolean drawOuterRing) {
