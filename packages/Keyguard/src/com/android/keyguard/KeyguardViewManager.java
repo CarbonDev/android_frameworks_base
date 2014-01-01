7bd340fc62ef83dd82aad600975b8ec37389c4a3
David96 <xd_e-mail@gmx.de>
2014-01-01 15:11:26 +0100

    Lockscreen Blur: fix for coverflows
    Change-Id: I668ab36cd846b30cdda1b70e2fca2c512211f72c

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -109,9 +109,9 @@ public class KeyguardViewManager {
     private KeyguardUpdateMonitorCallback mBackgroundChanger = new KeyguardUpdateMonitorCallback() {
         @Override
         public void onSetBackground(Bitmap bmp) {
-            mKeyguardHost.setCustomBackground( new BitmapDrawable(mContext.getResources(),
-                        bmp != null ? bmp : mBlurredImage) );
-            updateShowWallpaper(bmp == null && mBlurredImage == null);
+            if (bmp != null) mBlurredImage = null;
+            mIsCoverflow = true;
+            setCustomBackground (bmp);
         }
     };
 
@@ -225,6 +225,12 @@ public class KeyguardViewManager {
         Resources res = mContext.getResources();
         return res.getBoolean(R.bool.config_enableLockScreenTranslucentDecor)
             && res.getBoolean(R.bool.config_enableTranslucentDecor);
+    }
+
+    private void setCustomBackground(Bitmap bmp) {
+        mKeyguardHost.setCustomBackground( new BitmapDrawable(mContext.getResources(),
+                    bmp != null ? bmp : mBlurredImage) );
+        updateShowWallpaper(bmp == null && mBlurredImage == null);
     }
 
     public void setBackgroundBitmap(Bitmap bmp) {
@@ -460,7 +466,7 @@ public class KeyguardViewManager {
             mBlurredImage = rotateBmp(mBlurredImage, mLastRotation - currentRotation);
             mLastRotation = currentRotation;
             mIsCoverflow = false;
-            KeyguardUpdateMonitor.getInstance(mContext).dispatchSetBackground(mBlurredImage);
+            setCustomBackground(mBlurredImage);
         } else {
             mIsCoverflow = true;
         }
