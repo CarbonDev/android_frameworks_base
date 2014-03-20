4c5059c8ad987afccdc68075ab1aef6e75a261da
Lars Greiss <kufikugel@googlemail.com>
2014-03-20 23:13:47 +0100

    Frameworks: Fix possible ANR and navigation bar freeze
    Big thanks to ralpheboyo which provided us the first time a usable
    anr trace where we can see what the actual problem is.
    
    The random ANR is caused due of to much work on the navigationBarCanMove
    feature. We call way to often the provider value even though it is only
    needed on initiliazing or on update of the value. Currently we call it on every
    possible navigation bar change and this multiple times.
    
    To fix it just call it on init and on contentobserver changes and pass
    the new value to the navigation bar view and the search panel view
    
    PS:
    - remove another call
    PS
    - add one security check just to be sure
    PS
    - get one step further and put out the navigation ring config as well
      and reduce it to just one call.
    
    Change-Id: Ica6116289d5bc50e9bedcd34b4b165062fa3468d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -314,6 +314,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // on-screen navigation buttons
     private NavigationBarView mNavigationBarView = null;
     private int mNavigationBarWindowState = WINDOW_STATE_SHOWING;
+    private boolean mNavigationBarCanMove;
+    private ArrayList<ButtonConfig> mNavigationRingConfig;
 
     // the tracker view
     int mTrackingPosition; // the position of the top of the tracking view.
@@ -560,10 +562,21 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.NAVIGATION_BAR_CAN_MOVE))
                 || uri.equals(Settings.System.getUriFor(
                     Settings.System.NAVRING_CONFIG))) {
-                prepareNavigationBarView();
+                mNavigationBarCanMove = DeviceUtils.isPhone(mContext) ?
+                        Settings.System.getIntForUser(mContext.getContentResolver(),
+                            Settings.System.NAVIGATION_BAR_CAN_MOVE, 1,
+                            UserHandle.USER_CURRENT) == 1
+                        : false;
+                mNavigationRingConfig = ButtonsHelper.getNavRingConfig(mContext);
                 if (mSearchPanelView != null) {
-                    mSearchPanelView.updateSettings();
+                    mSearchPanelView.setNavigationBarCanMove(mNavigationBarCanMove);
+                    mSearchPanelView.setNavigationRingConfig(mNavigationRingConfig);
+                    mSearchPanelView.setDrawables();
                 }
+                if (mNavigationBarView != null) {
+                    mNavigationBarView.setNavigationBarCanMove(mNavigationBarCanMove);
+                }
+                prepareNavigationBarView();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.PIE_CONTROLS))) {
                 attachPieContainer(isPieEnabled());
@@ -1337,8 +1350,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     @Override
-    protected void updateSearchPanel() {
-        super.updateSearchPanel();
+    protected void updateSearchPanel(boolean navigationBarCanMove,
+            ArrayList<ButtonConfig> buttonConfig) {
+        super.updateSearchPanel(navigationBarCanMove, buttonConfig);
         if (mNavigationBarView != null) {
             mNavigationBarView.setDelegateView(mSearchPanelView);
         }
@@ -1452,7 +1466,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         mNavigationBarView.getSearchLight().setOnTouchListener(mHomeSearchActionListener);
         setDisableHomeLongpress();
-        updateSearchPanel();
+        updateSearchPanel(mNavigationBarCanMove, mNavigationRingConfig);
     }
 
     // For small-screen devices (read: phones) that lack hardware navigation buttons
@@ -1466,6 +1480,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             // Nevermind, this will be re-created
             return;
         }
+
+        mNavigationBarCanMove = DeviceUtils.isPhone(mContext) ?
+                Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_CAN_MOVE, 1,
+                    UserHandle.USER_CURRENT) == 1
+                : false;
+        mNavigationRingConfig = ButtonsHelper.getNavRingConfig(mContext);
+
+        mNavigationBarView.setNavigationBarCanMove(mNavigationBarCanMove);
 
         prepareNavigationBarView();
 
@@ -3518,7 +3541,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 || Intent.ACTION_PACKAGE_ADDED.equals(action)) {
 
                 if (mSearchPanelView != null && mSearchPanelView.hasAppBinded()) {
-                    mSearchPanelView.updateSettings();
+                    mSearchPanelView.setDrawables();
                 }
                 if (mNavigationBarView != null && mNavigationBarView.hasAppBinded()) {
                     mNavigationBarView.recreateNavigationBar();
@@ -3556,17 +3579,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void updateSwapXY() {
         if (mNavigationBarView != null
                 && mNavigationBarView.mDelegateHelper != null) {
-            boolean navigationBarCanMove = DeviceUtils.isPhone(mContext) ?
-                    Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.NAVIGATION_BAR_CAN_MOVE, 1,
-                        UserHandle.USER_CURRENT) == 1
-                    : false;
             // if we are in landscape mode and NavBar
             // can move swap the XY coordinates for NaVRing Swipe
             mNavigationBarView.mDelegateHelper.setSwapXY(
                     mContext.getResources().getConfiguration()
                             .orientation == Configuration.ORIENTATION_LANDSCAPE
-                    && navigationBarCanMove);
+                    && mNavigationBarCanMove);
         }
     }
 
