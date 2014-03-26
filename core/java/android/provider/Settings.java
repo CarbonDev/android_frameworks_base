cab171e762e5ab610a614505a2698bddad7181f6
Lars Greiss <kufikugel@googlemail.com>
2014-03-26 21:59:39 +0100

    Recents RAM bar [1/2]
    Change-Id: Ib4591ff48473ec3c57ff574bb6a6cca2b6391a41
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3861,6 +3861,34 @@ public final class Settings {
         public static final String CLEAR_RECENTS_BUTTON = "clear_recents_button";
 
         /**
+         * whether which Ram Usage Bar mode is used on recent switcher
+         * 0 = none, 1 = only app use, 2 = app and cache use, 3 = app, cache and system use
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_MODE = "recents_ram_bar_mode";
+
+        /**
+         * Ram Usage Bar system mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_MEM_COLOR = "recents_ram_bar_mem_color";
+
+        /**
+         * Ram Usage Bar cached mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_CACHE_COLOR = "recents_ram_bar_cache_color";
+
+        /**
+         * Ram Usage Bar app mem color
+         *
+         * @hide
+         */
+        public static final String RECENTS_RAM_BAR_ACTIVE_APPS_COLOR = "recents_ram_bar_active_apps_color";
+
+        /**
          * Screenshot toggle delay
          * @hide
          */
