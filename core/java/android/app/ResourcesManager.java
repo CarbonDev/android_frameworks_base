05019fd5528582bc5a3527539ef64a8dedcd7d08
Clark Scheff <clark@cyngn.com>
2014-05-20 09:58:31 -0700

    CM11 Themes: Get proper package name in attachThemeAssets
    Change-Id: Ic77ff41519b22e3b20c4571785a1368cdf4d14d2

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -423,7 +423,9 @@ public class ResourcesManager {
         int count = assets.getBasePackageCount();
         if (count > 1) {
             packageName  = assets.getBasePackageName(1);
-        } else if (count <= 1) {
+        } else if (count == 1) {
+            packageName  = assets.getBasePackageName(0);
+        } else {
             return false;
         }
 
