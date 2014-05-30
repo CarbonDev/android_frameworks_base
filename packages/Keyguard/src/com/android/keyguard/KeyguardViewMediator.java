d7c8f30da2478cd0498911f24dad6ec240b50eb4
kaiyiz <kaiyiz@codeaurora.org>
2014-05-30 15:01:36 +0800

    Keyguard: Can't enter PIN code when setting the SIM and PIN lock issue
    By original design, after unlock SIM PIN, SIM card's state is READY and it
    will invoke resetStateLocked() to inflate KeyguardHostView. So there is
    only one view be created in mSecurityViewContainer.
    
    Roll back previous solution.
    Don't set mSimState before handleSimStateChange() and don't judge
    isSecure() to be consistent with the original design.
    
    CRs-Fixed: 661955
    
    Change-Id: Ide872c50e6823bed92720c0c40aa7987207980ed

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -449,7 +449,7 @@ public class KeyguardViewMediator {
                     break;
                 case READY:
                     synchronized (this) {
-                        if (isShowing() && !isSecure()) {
+                        if (isShowing()) {
                             resetStateLocked(null);
                         }
                     }
