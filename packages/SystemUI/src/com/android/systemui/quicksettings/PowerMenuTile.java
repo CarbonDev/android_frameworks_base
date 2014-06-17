7d46ef357a2751fcbaf01e4a3dda5faf8e28b2fe
Stefano Mura [STELIX PippoX3] <ssspinni@gmail.com>
2014-06-17 12:27:45 +0100

    FWBase - PowerMenuTile fix
    Change-Id: Ib28ab384cf5be1d909d7e622ed25f861e2701a99
    Signed-off-by: Stefano Mura [STELIX PippoX3] <STELIX@gmail.com>

--- packages/SystemUI/src/com/android/systemui/quicksettings/PowerMenuTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/PowerMenuTile.java
@@ -3,8 +3,12 @@ package com.android.systemui.quicksettings;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
@@ -18,9 +22,13 @@ public class PowerMenuTile extends QuickSettingsTile{
         mOnClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            	qsc.mBar.collapseAllPanels(true);
-                Intent intent = new Intent(Intent.ACTION_POWERMENU);
-                mContext.sendBroadcast(intent);
+                qsc.mBar.collapseAllPanels(true);
+                final IWindowManager windowManagerService = IWindowManager.Stub.asInterface(
+                    ServiceManager.getService(Context.WINDOW_SERVICE));
+                try {
+                    windowManagerService.toggleGlobalMenu();
+                } catch (RemoteException e) {
+                }
             }
         };
         mOnLongClick = new View.OnLongClickListener() {
