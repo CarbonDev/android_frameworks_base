a7a418bf2b22a16fbbed34b29708635c341e2645
Dan Pasanen <dan.pasanen@gmail.com>
2014-08-19 20:24:08 -0500

    ShutdownThread: set soft reboot trigger accordingly
    Change-Id: I72bd52aa1492d356e368e8c60897002c1f110172

--- services/java/com/android/server/power/ShutdownThread.java
+++ services/java/com/android/server/power/ShutdownThread.java
@@ -192,9 +192,7 @@ public final class ShutdownThread extends Thread {
 
                                     if (actions != null && which < actions.length) {
                                         mRebootReason = actions[which];
-                                        if (actions[which].equals(SOFT_REBOOT)) {
-                                            mRebootSoft = true;
-                                        }
+                                        mRebootSoft = actions[which].equals(SOFT_REBOOT);
                                     }
 
                                     if (mRebootSoft) {
